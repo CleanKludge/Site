2402c715da6d69c78b0c9ecb4e12db6a3c954619
Korthax <stephenneilphillips@gmail.com>
2017-04-29 22:32:56 +0100

    Fix for DI

--- src/CleanKludge.Data.File/Modules/FileModule.cs
+++ src/CleanKludge.Data.File/Modules/FileModule.cs
@@ -1,9 +1,11 @@
-﻿using CleanKludge.Core.Articles.Data;
+﻿using CleanKludge.Core.Articles;
+using CleanKludge.Core.Articles.Data;
 using CleanKludge.Data.File.Articles;
 using CleanKludge.Data.File.Serializers;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
 namespace CleanKludge.Data.File.Modules
 {
@@ -13,8 +15,8 @@ namespace CleanKludge.Data.File.Modules
         {
             self.TryAddSingleton<IMemoryCache>(provider => new MemoryCache(new MemoryCacheOptions()));
             self.TryAddSingleton<ISerializer, JsonSerializer>();
-            self.TryAddSingleton<IArticlePath, ArticlePath>();
-            self.TryAddSingleton<ISummaryPath, SummaryPath>();
+            self.TryAddSingleton<IArticlePath>(x => ArticlePath.For(x.GetService<IOptions<ContentOptions>>()));
+            self.TryAddSingleton<ISummaryPath>(x => SummaryPath.For(x.GetService<IOptions<ContentOptions>>()));
             self.TryAddSingleton<IArticleSummaryRepository, ArticleSummaryRepository>();
             self.TryAddSingleton<IArticleRepository, ArticleRepository>();
             return self;
