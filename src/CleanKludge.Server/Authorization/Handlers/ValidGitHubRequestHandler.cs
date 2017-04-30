0eee5fa989db1e28ffae4d7bdbb9a918959c9b56
Korthax <stephenneilphillips@gmail.com>
2017-04-30 13:11:50 +0100

    More logging around github authorization.

--- src/CleanKludge.Server/Authorization/Handlers/ValidGitHubRequestHandler.cs
+++ src/CleanKludge.Server/Authorization/Handlers/ValidGitHubRequestHandler.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using Serilog;
 
 namespace CleanKludge.Server.Authorization.Handlers
 {
@@ -15,9 +16,11 @@ namespace CleanKludge.Server.Authorization.Handlers
     {
         private const string AuthenticationType = "sha1=";
         private readonly GitHubOptions _options;
+        private readonly ILogger _logger;
 
-        public ValidGitHubRequestHandler(IOptions<GitHubOptions> options)
+        public ValidGitHubRequestHandler(IOptions<GitHubOptions> options, ILogger logger)
         {
+            _logger = logger.ForContext<ValidGitHubRequestHandler>();
             _options = options?.Value ?? new GitHubOptions();
         }
 
@@ -42,26 +45,36 @@ namespace CleanKludge.Server.Authorization.Handlers
                 context.Fail();
         }
 
-        public static bool Validate(string signature, string eventType, string delivery, string body, string token)
+        public bool Validate(string signature, string eventType, string delivery, string body, string token)
         {
-            if (string.IsNullOrWhiteSpace(eventType) || string.IsNullOrWhiteSpace(delivery) || string.IsNullOrWhiteSpace(signature))
-                return false;
-
-            if (!eventType.Equals("push", StringComparison.OrdinalIgnoreCase))
-                return false;
-
-            if (!signature.StartsWith(AuthenticationType, StringComparison.OrdinalIgnoreCase))
-                return false;
-
-            var signatureData = signature.Substring(AuthenticationType.Length);
-            var secret = Encoding.ASCII.GetBytes(token);
-            var payloadBytes = Encoding.ASCII.GetBytes(body);
-
-            using (var hmacsha1 = new HMACSHA1(secret))
+            try
             {
-                var hash = hmacsha1.ComputeHash(payloadBytes);
-                var hashString = hash.ToHexString();
-                return hashString.Equals(signatureData);
+                _logger.Information("Validating {Signature} for {EventType} with {Token} on {Body}", signature ?? "null", eventType ?? "null", token ?? "null", body ?? "null");
+
+                if (string.IsNullOrWhiteSpace(eventType) || string.IsNullOrWhiteSpace(delivery) || string.IsNullOrWhiteSpace(signature))
+                    return false;
+
+                if (!eventType.Equals("push", StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                if (!signature.StartsWith(AuthenticationType, StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                var signatureData = signature.Substring(AuthenticationType.Length);
+                var secret = Encoding.ASCII.GetBytes(token);
+                var payloadBytes = Encoding.ASCII.GetBytes(body);
+
+                using (var hmacsha1 = new HMACSHA1(secret))
+                {
+                    var hash = hmacsha1.ComputeHash(payloadBytes);
+                    var hashString = hash.ToHexString();
+                    return hashString.Equals(signatureData);
+                }
+            }
+            catch(Exception exception)
+            {
+                _logger.Error(exception, "Failed to validate {Signature} for {EventType} with {Token} on {Body}", signature, eventType, token, body);
+                return false;
             }
         }
     }
